0ece1b2941e4315e65e76090a1da75445d74ae49
Usman Rafique <140150452+usmannrafiq@users.noreply.github.com>
2025-09-15 14:10:19 -0400

    Add solution for binary-search

---
+++ leetcode/binary-search.py
@@ -0,0 +1,28 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        # initialize two pointers left and right
+        l, r = 0, len(nums) - 1
+
+        # iterate through the loop until left is greater than right
+        while l <= r:
+            
+            # find the midpoint of the list 
+            midpoint = r + l // 2
+
+            # if target is greater than the midpoint, set the left greater than half
+            if target > nums[midpoint]:
+                l = midpoint + 1
+             
+            # if target is smaller than the midpoint, set the right less than half
+            elif target < nums[midpoint]:
+                r = midpoint - 1
+            
+            # loop will iterate until midpoint will be equal to target so return midpoint
+            # The last iteration will be when left, right and midpoint are equal and in
+            # the next iteration the loop breaks
+            else:
+                return midpoint
+        
+        # if there is no target value in the loop
+        return -1
